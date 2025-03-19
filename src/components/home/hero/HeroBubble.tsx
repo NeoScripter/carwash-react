eeadc8948dd1f83290a870e73c72ef2dcebe7cf3
Ilya <sange0337@gmail.com>
2025-03-19 23:23:10 +0800

    build after refactoring

--- src/components/home/hero/HeroBubble.tsx
+++ src/components/home/hero/HeroBubble.tsx
@@ -9,7 +9,7 @@ export default function HeroBubble({ children, className }: HeroBubbleProps) {
     return (
         <div
             className={clsx(
-                'items-center text-black-20 justify-center w-4/5 mx-auto bg-gray-10 rounded-full aspect-square z-20 shadow-logo sm:flex-1',
+                'items-center text-black justify-center w-4/5 mx-auto bg-gray-10 rounded-full aspect-square z-20 shadow-logo sm:flex-1',
                 className
             )}
         >
